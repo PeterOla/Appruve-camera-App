56f493910ba90e41d3a3fd182195442ecbceee90
Peter Olaleru <Olalerupeter@gmail.com>
2020-05-11 18:11:34 +0100

    upload commit

--- app/src/main/java/com/hollainc/appruvecamera/ImageFragment.java
+++ app/src/main/java/com/hollainc/appruvecamera/ImageFragment.java
@@ -1,10 +1,11 @@
 package com.hollainc.appruvecamera;
 
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,37 +16,45 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.ByteArrayBody;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 import java.util.UUID;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class ImageFragment extends Fragment {
 
     private Bitmap bitmap;
     private String path;
-
+    private File file;
     @BindView(R.id.res_photo)
     ImageView resPhoto;
-
+    private String ENDPOINT = "https://stage.appruve.co/v1/verifications/test/file_upload";
+    ProgressDialog p;
+    OkHttpClient client;
 
     public void imageSetupFragment(Bitmap bitmap, String path) {
         if (bitmap != null) {
             this.bitmap = bitmap;
             this.path = path;
+
         }
     }
 
@@ -53,8 +62,7 @@ public class ImageFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setRetainInstance(true);
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
+
     }
 
     @Override
@@ -63,149 +71,76 @@ public class ImageFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_image, container, false);
         ButterKnife.bind(this, view);
+        client = new OkHttpClient();
+
         if (bitmap != null) {
             resPhoto.setImageBitmap(bitmap);
         }
 
 
         view.findViewById(R.id.sendBtn).setOnClickListener(v -> {
-            String uniqueID = UUID.randomUUID().toString();
-            String ENDPOINT = "https://stage.appruve.co/v1/verifications/test/file_upload";
-            Map<String, String> params = new HashMap<>(2);
-            params.put("user_id", uniqueID);
 
             try {
-                String result = multipartRequest(ENDPOINT, params, path, "image", "image/jpg");
-                Log.v("APPRUVE", result);
 
-                Intent i = new Intent(getActivity(), MainActivity.class);
-                startActivity(i);
+                file = new File(path);
+                new ServerPushAsync().execute(ENDPOINT);
 
             } catch (Exception e) {
                 Toast.makeText(getActivity(), "H" + e, Toast.LENGTH_SHORT).show();
                 e.printStackTrace();
             }
         });
-
-
         return view;
     }
 
 
-    public String multipartRequest(String urlTo, Map<String, String> parmas, String filepath, String filefield, String fileMimeType) throws Exception {
-
-
-        HttpURLConnection connection = null;
-        DataOutputStream outputStream = null;
-        InputStream inputStream = null;
-
-        String twoHyphens = "--";
-        String boundary = "*****" + Long.toString(System.currentTimeMillis()) + "*****";
-        String lineEnd = "\r\n";
-
-        String result = "";
-
-        int bytesRead, bytesAvailable, bufferSize;
-        byte[] buffer;
-        int maxBufferSize = 1 * 1024 * 1024;
-
-        String[] q = filepath.split("/");
-        int idx = q.length - 1;
-
-        try {
-            File file = new File(filepath);
-            FileInputStream fileInputStream = new FileInputStream(file);
-
-            URL url = new URL(urlTo);
-            connection = (HttpURLConnection) url.openConnection();
-
-            connection.setDoInput(true);
-            connection.setDoOutput(true);
-            connection.setUseCaches(false);
-
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Connection", "Keep-Alive");
-            connection.setRequestProperty("User-Agent", "Android Multipart HTTP Client 1.0");
-            connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
-
-            outputStream = new DataOutputStream(connection.getOutputStream());
-            outputStream.writeBytes(twoHyphens + boundary + lineEnd);
-            outputStream.writeBytes("Content-Disposition: form-data; name=\"" + filefield + "\"; filename=\"" + q[idx] + "\"" + lineEnd);
-            outputStream.writeBytes("Content-Type: " + fileMimeType + lineEnd);
-            outputStream.writeBytes("Content-Transfer-Encoding: binary" + lineEnd);
-
-            outputStream.writeBytes(lineEnd);
-
-            bytesAvailable = fileInputStream.available();
-            bufferSize = Math.min(bytesAvailable, maxBufferSize);
-            buffer = new byte[bufferSize];
-
-            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-            while (bytesRead > 0) {
-                outputStream.write(buffer, 0, bufferSize);
-                bytesAvailable = fileInputStream.available();
-                bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-            }
-
-            outputStream.writeBytes(lineEnd);
-
-            // Upload POST Data
-            Iterator<String> keys = parmas.keySet().iterator();
-            while (keys.hasNext()) {
-                String key = keys.next();
-                String value = parmas.get(key);
-
-                outputStream.writeBytes(twoHyphens + boundary + lineEnd);
-                outputStream.writeBytes("Content-Disposition: form-data; name=\"" + key + "\"" + lineEnd);
-                outputStream.writeBytes("Content-Type: text/plain" + lineEnd);
-                outputStream.writeBytes(lineEnd);
-                outputStream.writeBytes(value);
-                outputStream.writeBytes(lineEnd);
-            }
-
-            outputStream.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
-
-
-            if (200 != connection.getResponseCode()) {
-                throw new Exception("Failed to upload code:" + connection.getResponseCode() + " " + connection.getResponseMessage());
-            }
-
-            inputStream = connection.getInputStream();
-
-            result = this.convertStreamToString(inputStream);
-
-            fileInputStream.close();
-            inputStream.close();
-            outputStream.flush();
-            outputStream.close();
-
-            return result;
-        } catch (Exception e) {
-//            L.error(e);
-            throw new Exception(e);
+    private class ServerPushAsync extends AsyncTask<String, String, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            p = new ProgressDialog(getActivity());
+            p.setMessage("Uploading.. Please wait...");
+            p.setIndeterminate(false);
+            p.setCancelable(false);
+            p.show();
         }
 
-    }
-
-    private String convertStreamToString(InputStream is) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        StringBuilder sb = new StringBuilder();
-
-        String line = null;
-        try {
-            while ((line = reader.readLine()) != null) {
-                sb.append(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
+        @Override
+        protected String doInBackground(String... strings) {
             try {
-                is.close();
-            } catch (IOException e) {
+                String endPoint = strings[0];
+
+                String uniqueID = UUID.randomUUID().toString();
+
+                RequestBody formBody = new MultipartBody.Builder()
+                        .setType(MultipartBody.FORM)
+                        .addFormDataPart("document", file.getName(),
+                                RequestBody.create(MediaType.parse("image/png"), file))
+                        .addFormDataPart("user_id", uniqueID)
+                        .build();
+                Request request = new Request.Builder().url(endPoint).post(formBody).build();
+                Response response = client.newCall(request).execute();
+
+                Log.e("APPRUVE_LOG", response.body().string());
+                return response.body().string();
+            } catch (Exception e) {
+                Log.e("APPRUVE_LOG", e.getMessage());
                 e.printStackTrace();
             }
+            return "";
         }
-        return sb.toString();
+
+        @Override
+        protected void onPostExecute(String response) {
+            super.onPostExecute(response);
+
+
+            p.hide();
+            Toast.makeText(getActivity(), "Uploaded", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(getActivity(), MainActivity.class);
+            startActivity(i);
+        }
     }
+
+
 }
